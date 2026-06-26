91f9f32ee2549a193e477d2892a1caad84284c7c
Marek <marekperzylo@gmail.com>
2026-06-26 21:15:29 +0200

    chi

--- world.py
+++ world.py
@@ -197,7 +197,7 @@ class World(BuildingsMixin):
             print(f"Błąd ładowania dodatkowych kafelków: {e}")
 
         # Inicjalizacja Graczy z przypisaniem frakcji
-        num_players = 2 
+        num_players = 5 
         player_data = [
             ("Don Marek", (200, 0, 0), "red", "catholic"),  # Katolik
             ("Lech VI", (0, 0, 200), "blue", "pagan"),      # Poganin
